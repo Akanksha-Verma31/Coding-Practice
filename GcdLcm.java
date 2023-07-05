fd99e991b3a3a1af54eff0ef3f71723e8c059cf1
Akanksha Verma <22906.akanksha@gmail.com>
2023-07-05 23:48:59 +0530

    Added gcd lcm

---
+++ GcdLcm.java
@@ -0,0 +1,48 @@
+/*
+1. You are required to print the Greatest Common Divisor (GCD) of two numbers.
+2. You are also required to print the Lowest Common Multiple (LCM) of the same numbers.
+3. Take input "num1" and "num2" as the two numbers.
+4. Print their GCD and LCM.
+
+Sample Input
+36
+24
+Sample Output
+12
+72
+ */
+
+import java.util.Scanner;
+
+public class GcdLcm {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        // Input the numbers
+        int num1 = scanner.nextInt();
+        int num2 = scanner.nextInt();
+
+        // Calculate GCD and LCM
+        int gcd = calculateGcd(num1, num2);
+        int lcm = calculateLcm(num1, num2);
+
+        // Print the results
+        System.out.println("GCD: " + gcd);
+        System.out.println("LCM: " + lcm);
+    }
+
+    private static int calculateGcd(int a, int b) {
+        while (b != 0) {
+            int temp = b;
+            b = a % b;
+            a = temp;
+        }
+        return a;
+    }
+
+    // Method to calculate the Lowest Common Multiple (LCM) using the GCD formula
+    private static int calculateLcm(int a, int b) {
+        return (a * b) / calculateGcd(a, b);
+    }
+}
