9861e8ded2909a17e4c7750820c31bf319e36ce6
Akanksha Verma <22906.akanksha@gmail.com>
2023-07-11 18:11:07 +0530

    Added Star Pattern 6

---
+++ pattern6.java
@@ -0,0 +1,63 @@
+/*
+*	*	*		*	*	*	
+*	*				*	*	
+*						*	
+*	*				*	*	
+*	*	*		*	*	*	
+ */
+
+import java.util.Scanner;
+
+public class pattern6 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int m = n / 2;
+        int count = 1;
+
+        // for printing the upper half
+        for (int i = Math.abs(m); i > 0; i--) {
+            if (i <= Math.abs(m)) {
+                for (int k = i; k >= 0; k--) {
+                    System.out.print("*");
+                }
+            }
+            if (count % 2 != 0) {
+                for (int k = 1; k <= count; k++) {
+                    System.out.print(" ");
+                }
+            }
+            if (i <= Math.abs(m)) {
+                for (int k = i; k >= 0; k--) {
+                    System.out.print("*");
+                }
+            }
+            count = count + 2;
+            System.out.println();
+        }
+
+        System.out.println();
+        count = Math.abs(m) + 1;
+
+        // for printing the lower half
+        for (int i = 1; i <= Math.abs(m) + 1; i++) {
+            if (i <= Math.abs(m)) {
+                for (int k = 0; k <= i; k++) {
+                    System.out.print("*");
+                }
+            }
+            if (count % 2 != 0) {
+                for (int k = count; k > 0; k--) {
+                    System.out.print(" ");
+                }
+            }
+            if (i <= Math.abs(m)) {
+                for (int k = i; k >= 0; k--) {
+                    System.out.print("*");
+                }
+            }
+            count = count - 2;
+            System.out.println();
+        }
+    }
+}
