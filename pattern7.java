cf0e54e5e876e7e2a4720e13c79798e10990587f
Akanksha Verma <22906.akanksha@gmail.com>
2023-07-11 18:39:51 +0530

    Added Star Pattern 7

---
+++ pattern7.java
@@ -0,0 +1,27 @@
+/*
+*	
+	*	
+		*	
+			*	
+				*	
+ */
+
+import java.util.Scanner;
+
+public class pattern7 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int space = 0;
+        for (int i = 1; i <= n; i++) {
+            if (space > 0) {
+                for (int j = 1; j <= space; j++)
+                    System.out.print(" ");
+            }
+            space++;
+            System.out.print("*\n");
+        }
+
+        sc.close();
+    }
+}
